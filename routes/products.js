35b462f51f24662f9ba2952d49e670c4724ff121
sohail0992 <msohail.se@gmail.com>
2018-12-05 12:37:24 +0500

    latest

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
 var productsController = require('../controllers/productController');
 
 router.get('/category', function (req, res) {
